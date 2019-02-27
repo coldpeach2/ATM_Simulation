ecfad0bcfee31e12cee31cdf0c7926961fdde121
uzoukwut <chiora.mba.uzoukwu@mail.utoronto.ca>
2019-02-26 19:50:40 -0500

    Made basic attributes and getters/setters for User class

--- phase1/User.java
+++ phase1/User.java
@@ -1,4 +1,62 @@
+import java.util.ArrayList;
+
 public class User {
 
-    User(){}
+    private String name;
+    private final String id; // id should never be changed
+    private String password;
+
+    //TODO: need an Account superclass to implement this ArrayList.
+    /*private ArrayList<Account> accounts;*/
+
+
+    /**
+     * Basic constructor for User class.
+     * Every user should have a Name, id & Password.
+     *
+     * @param name   User's full name
+     * @param id    a unique character sequence associated with this User
+     * @param password User's password
+     */
+
+
+    User(String name, String id, String password){
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    /*
+    Writes a given transaction to a transactions.txt file.
+
+    A line in transactions.txt is a single transaction, separated by semi-colons:
+    (<type of transaction>; <money from>; <amount from>; <money to>; <amount to>)
+
+     */ //TODO: define how transactions will appear on file.
+    //TODO: should this method be in this class?
+    private void writeTransaction(String type) {}
+
 }
+
+
